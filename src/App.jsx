e4e4742f414c964bacc56d09db59b05f3320d43f
Larysa Rud <larpavrud@gmail.com>
2024-09-01 09:20:15 -0400

    Adding styling

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,9 @@ function App() {
   return (
     <>
        <div className="App">
+      <section className="App-section">
         <PomodoroTimer />
+      </section>
     </div>
     </>
   )
--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import './Timer.css';
 
 // Defining functional React component PomodoroTimer:
 const PomodoroTimer = () => {
